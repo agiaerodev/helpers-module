d74564fce706a572419a377eb1b13720a20b3c55
Juan Escalante <jcec007@gmail.com>
2026-01-22 01:30:58 -0500

    Update: new Data Anotation ApiName for models to mask the field name

---
+++ DataAnotations/ApiName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Ihelpers.DataAnotations
+{
+    public class ApiName : ValidationAttribute, IDataAnnotationBase
+    {
+        public string? _label { get; } = null;
+
+
+
+        public ApiName( string? label)
+        {
+
+            _label = label;
+        }
+
+        /// <summary>
+        /// Returns a success result, indicating that the validation should not be converted to the user's timezone.
+        /// </summary>
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            return ValidationResult.Success;
+        }
+    }
+
+}
